f86f0091920e444715e3417f9c8f48b02239f76b
AlexUKDev <61418792+AlexUKDev@users.noreply.github.com>
2022-08-14 15:40:52 +0300

    Script for Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,65 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('ul.gallery');
+
+function createHtmlTamplate({preview, original, description }) {
+  let htmlTamplate = 
+    `<li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+    `;
+  
+  return htmlTamplate;
+}
+
+function renderFullTamplate() {
+   const fullTamplate = galleryItems.map((item)=> {
+    return createHtmlTamplate(item);
+  }).join('');
+
+  // console.log(fullTamplate);
+
+  galleryContainerRef.insertAdjacentHTML('beforeend', fullTamplate);
+}
+
+renderFullTamplate();
+
+galleryContainerRef.addEventListener('click', onImgClick)
+document.addEventListener('click', onBodyClick)
+
+function onImgClick(e){
+  // отменяем дефолтное поведение ссылок
+  e.preventDefault();
+
+  //  фильтр цели делигации
+  if (e.target.nodeName !== "IMG"){
+    return
+  }
+
+  console.log(e.target)
+
+    // получаем ссылку большого размера для отображения в галерее
+  let originalImgUrl = e.target.dataset.source;
+
+  let gallery = new SimpleLightbox('.gallery a', {  captionType:'attr',captionsData: "alt", captionDelay: 250 });
+gallery.on('show.simplelightbox', function () {
+	// Do something…
+});
+  console.log(gallery)
+
+}
+
+
+function onBodyClick(evt){
+  console.log(evt.target)
+
+  // clases sl-wrapper simple-lightbox
+  // sl-close
+}
+
